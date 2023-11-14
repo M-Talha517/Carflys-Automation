566c360b81db35e91924253f75e355f4f26a5695
UsamaKhalid123 <usamaabbasi1996@outlook.com>
2023-11-14 12:53:15 +0500

    github token  added 1

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "1h9e4k",
+    projectId: "rudmb7",
     setupNodeEvents(on, config) {
       // ok
     },
